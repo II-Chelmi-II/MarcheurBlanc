4108b8c9b0cd9a4ea1f149a172dd368ebc32b240
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-06-17 19:44:24 +0300

    fix: Fix MarcheurService logic for dead ends

--- src/main/java/service/MarcheurService.java
+++ src/main/java/service/MarcheurService.java
@@ -7,8 +7,7 @@ import model.Carte;
 import model.Lieu;
 import model.Rue;
 
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @Data
 @AllArgsConstructor
@@ -24,14 +23,40 @@ public class MarcheurService {
         this.random = new Random();
     }
 
-    public void marcher() {
-        List<Rue> rues = carte.getRues(positionActuelle);
-        if (!rues.isEmpty()) {
-            Rue prochaineRue = rues.get(random.nextInt(rues.size()));
-            positionActuelle = prochaineRue.getDestination();
-            System.out.println("Marcher vers: " + positionActuelle);
-        } else {
-            System.out.println("Aucune rue disponible depuis " + positionActuelle);
+    public void marcherVers(Lieu destination) {
+        Stack<Lieu> pile = new Stack<>();
+        Set<Lieu> visites = new HashSet<>();
+        pile.push(positionActuelle);
+        visites.add(positionActuelle);
+
+        while (!pile.isEmpty()) {
+            Lieu lieuCourant = pile.peek();
+            if (lieuCourant.equals(destination)) {
+                positionActuelle = lieuCourant;
+                System.out.println("Arrivé à destination: " + lieuCourant);
+                return;
+            }
+
+            List<Rue> rues = carte.getRues(lieuCourant);
+            List<Rue> ruesNonVisitees = new ArrayList<>();
+            for (Rue rue : rues) {
+                if (!visites.contains(rue.getDestination())) {
+                    ruesNonVisitees.add(rue);
+                }
+            }
+
+            if (!ruesNonVisitees.isEmpty()) {
+                Rue prochaineRue = ruesNonVisitees.get(random.nextInt(ruesNonVisitees.size()));
+                Lieu prochainLieu = prochaineRue.getDestination();
+                visites.add(prochainLieu);
+                pile.push(prochainLieu);
+                System.out.println("Marcher vers: " + prochainLieu);
+            } else {
+                System.out.println("Cul-de-sac à: " + lieuCourant + ", revenir en arrière");
+                pile.pop();
+            }
         }
+
+        System.out.println("Aucun chemin trouvé vers " + destination);
     }
 }
